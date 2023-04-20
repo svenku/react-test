d8713eac5ad048e64229ffad14dd3e9aa37c9a5f
svenku <sven.kunsing@gmail.com>
2023-04-21 01:12:51 +0300

    Render New ExpenseForm conditionally

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,34 @@
+import { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
 import ExpenseForm from "./ExpenseForm";
 import Card from "../UI/Card";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: uuidv4()
     };
     props.onAddExpense(expenseData); // pass the expense data to the parent component
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <Card className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && <button onClick={startEditingHandler}>Add new expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
     </Card>
   );
 };
